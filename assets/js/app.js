05fcc8b7af2a677d33f4c5faf7574e3c0db69913
gillesMifsud <gil.mifsud@gmail.com>
2018-09-23 17:56:03 +0200

    css

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,4 +4,6 @@ const $ = require('jquery');
 require('what-input');
 require('foundation-sites');
 
+import css from '../scss/app.scss';
+
 $(document).foundation();
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 const uglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const ENV = process.env.NODE_ENV;
 
+// https://www.youtube.com/watch?v=g11Pty544zo&list=PLjwdMgw5TTLVzGXGxEBdjwHXCeYnBb7n8&index=5
+// 9min
+
 let config = {
     mode: "development", // "production" | "development" | "none"  // Chosen mode tells webpack to use its built-in optimizations accordingly.
     entry: './assets/js/app.js',
@@ -18,6 +21,30 @@ let config = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    { loader: 'css-loader', options: {importLoaders: 1} },
+                    { loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: (loader) => [
+                                require('postcss-import')({root: loader.resourcePath}),
+                                require('postcss-preset-env')(),
+                                require('autoprefixer')({
+                                    browsers: ["last 4 versions", "ie > 10"]
+                                })
+                            ]
+                        }
+                    },
+                    'sass-loader'
+                ]
             }
         ]
     },
@@ -25,15 +52,16 @@ let config = {
         minimizer: []
     },
     performance: {
-        hints: ENV === 'production' ? "warning" : false
+        hints: false
     },
 };
 
 module.exports = (env, argv) => {
 
     if (argv.mode === 'development') {
-        config.devtool = 'source-map';
-        config.watch = true
+        config.devtool = 'cheap-eval-source-map';
+        config.watch = true;
+        config.performance.hints = "warning";
     }
 
     if (argv.mode === 'production') {
@@ -47,7 +75,8 @@ module.exports = (env, argv) => {
                 ],
                 exclude: [/node_modules\/(?!(foundation-sites)\/).*/, /node_modules/],
             })
-        )
+        );
+        config.performance.hints = false;
     }
 
     return config;
